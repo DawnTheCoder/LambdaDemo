e5dab411066877f11c365401c54ddad06c54781c
陈鹏飞 <chenpengfei@bmkp.cn>
2017-11-29 15:37:01 +0800

    增加了Lombok框架，以及示例的bean

---
+++ src/main/java/com/dawn/model/Student.java
@@ -0,0 +1,15 @@
+package com.dawn.model;
+
+import lombok.Data;
+
+/**
+ * @author Chenpengfei
+ * Created on 2017/11/29
+ */
+
+@Data
+public class Student {
+    private String name;
+    private Integer age;
+    private Integer score;
+}
